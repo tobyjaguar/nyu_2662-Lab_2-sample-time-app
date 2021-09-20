578b59ac33e507babbbb7f8fb00784fa3ae9f91a
tobyjaguar <talgya@gmail.com>
2021-09-19 20:35:02 -0400

    push from course repo with solutions

---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/time')
+def time():
+    current_time = datetime.now().strftime('%H:%M:%S')
+    return "<h1>" + current_time + "</h1>"
+
+app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
---
+++ time_check.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+def text():
+    print(datetime.now().strftime('%H:%M:%S'))
+    # print(datetime.now())
+
+text()
